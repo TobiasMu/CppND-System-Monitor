a4927bfc8e79fd47c282a084e9a72836d32d3e1a
TobiasMu <64594392+TobiasMu@users.noreply.github.com>
2025-11-01 06:58:41 +0100

    memtest

--- mock_proc/memtest.cpp
+++ mock_proc/memtest.cpp
@@ -6,19 +6,19 @@
 
 
 int main(){
-  std::ifstream filestream("stat");
+  std::ifstream filestream("meminfo");
 
   if (filestream.is_open()){
     std::string line;
     while(std::getline(filestream, line)){
       std::istringstream linestream(line);
-      std::string a,b,c,d,e,f;
-      linestream >> a >> b >> c>> d;
-      std::cout << a <<b << c <<d << std::endl;
+      std::string a,b,c,d,e,f,g;
+      double va = 0.0, vb, vc;
+      linestream >> a >> b >> c >> d;
+      vb = std::stoi(b);
       
+      std::cout << a << b << std::endl;
       
-        }
- 
       }
   }
-
+}
